4cf8924f4bd742c6d36702042df0d45fb63906c1
Cellingo <Cellingothefox@gmail.com>
2026-05-09 02:14:09 +0500

    1st commit

--- variables.cpp
+++ variables.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 
-int main()
+int main(){
+
+    int x; //declaration
+    x = 15; //assignment
+    int y = 125; //both declaration and assignment
+    int sum = x + y; //declaration, assignment with included operation
+    std::cout << x << '\n'; //output
+    std::cout << y << '\n';
+    std::cout << sum;
+    return 0;
+
+}
