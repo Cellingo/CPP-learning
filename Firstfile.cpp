5d407318e98195a748f6487039f8fffe8f14b263
Cellingo <Cellingothefox@gmail.com>
2026-05-09 01:58:13 +0500

    Initial commit

---
+++ Firstfile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    //This is a comment
+    /*This
+    Is
+    a
+    Multiline
+    Comment
+    */
+
+    std::cout << "I like pizza" << '\n';
+    std::cout << "Especially with bacon and ham";
+    return 0;
+}
